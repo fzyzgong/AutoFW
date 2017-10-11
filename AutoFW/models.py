b1b70a79e4bad4db2569f77a286231982ada3499
fzyzgong <493305356@qq.com>
2017-10-11 16:06:54 +0800

    mysql-login-check

--- AutoFW/models.py
+++ AutoFW/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class UserInfo(models.Model):
+    id = models.IntegerField
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=100)
+    authority = models.CharField(max_length=20)#super/common/guest
+    createtime = models.DateTimeField()
+    remark = models.CharField(max_length=100)
+    position = models.CharField(max_length=20)
+    gender = models.BooleanField(default=False)
--- AutoFW/views.py
+++ AutoFW/views.py
@@ -1,7 +1,42 @@
 #coding=utf-8
 from django.shortcuts import render,redirect
 from django.http import HttpResponse,JsonResponse
-
+from models import *
+import MySQLdb
 
 def login(request):
-    return render(request, 'AutoFW/index.html')
+    return render(request, 'AutoFW/login.html')
+
+
+def login_handle(request):
+    post = request.POST.get
+    user = post.get('username')
+    passwd = post.get('password')
+
+    users = UserInfo.objects.filter(username=user)
+
+    conn = MySQLdb.connect(host='localhost',port=3306,db='autofw',user='root',passwd='123456',charset='utf8')
+
+    handle = conn.cursor()
+
+    username = handle.execute("select username from AutoFW_userinfo where username='%s'" % user)
+    username = handle.fetchall()
+
+    password = handle.execute("select password from AutoFW_userinfo where username='%s'" % user)
+    password = handle.fetchall()
+
+    print ("username:"+username+"  password:"+password)
+
+    #判断元组是否为空，空为false
+    if(username and password):
+        if(username[0][0]==user and password[0][0]==passwd):
+            username = username[0][0]
+            position = UserInfo.objects.filter(username=username)
+            position = position[0][0]
+
+            context = {'username':username,'position':position}
+
+            return render(request,'AutoFW/workbench.html')
+
+
+    return HttpResponse("hello")
